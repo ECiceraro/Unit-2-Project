15463fcd36ad01341ab9865811d2b26d9bbe4490
Erik Ciceraro <erikciceraro@Eriks-MacBook-Pro.local>
2019-11-05 22:56:29 -0500

    finished 7 routes for the recipes

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -3,12 +3,6 @@ const express = require('express');
 const router = express.Router();
 const Recipes = require('../models/recipes.js')
 
-
-
-
-
-
-
 //_______________________
 // Routes
 //_______________________
@@ -19,25 +13,67 @@ router.get('/new', (req,res) => {
 })
 
 // Recipe Index Page
-router.get('/', (req,res) =>{
-    res.render('recipes/index.ejs');
+router.get('/', (req,res) => {
+    Recipes.find({}, (error, allRecipes) => {
+        res.render(
+            'recipes/index.ejs',
+            {
+                recipes: allRecipes
+            }
+        );
+    })
 })
 
 // Show Routes
 router.get('/:id', (req,res) => {
-    res.render('recipes/show.ejs')
+    Recipes.findById(req.params.id, (error, foundRecipe) => {
+        res.render(
+            'recipes/show.ejs',
+            {
+                recipe: foundRecipe
+            }
+        )
+    })
 })
 
 // Create/Post Route
 router.post('/', (req,res) => {
     Recipes.create(req.body, (error, createdRecipe) => {
-        res.redirect('recipes/index.ejs')
+        res.redirect('/recipes')
     })
 })
 
+// Edit Route
+router.get('/:id/edit', (req,res) => {
+    Recipes.findById(req.params.id, (error, editedRecipe) => {
+        res.render(
+            'recipes/edit.ejs',
+            {
+                recipe: editedRecipe
+            }
+        )
+    })
+})
 
+// Update Route
+router.put('/:id', (req,res) => {
+    Recipes.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        {new:true}, (error, updatedRecipe) => {
+            res.redirect('/recipes')
+        }
+    )
+})
 
-
+// Delete Route
+router.delete('/:id', (req,res) => {
+    Recipes.findByIdAndRemove(
+        req.params.id,
+        (error, deletedRecipe) => {
+            res.redirect('/recipes')
+        })
+})
 
 
 module.exports = router;
