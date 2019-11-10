861c0ea28a6ad647e6e883f04c1421ba60ac11b3
Erik Ciceraro <erikciceraro@Eriks-MacBook-Pro.local>
2019-11-10 12:01:20 -0500

    seeded the recipes and continued to style

--- models/recipes.js
+++ models/recipes.js
@@ -2,7 +2,14 @@ const mongoose = require('mongoose');
 
 const recipeSchema = new mongoose.Schema({
     name: { type: String, required: true, unique: true },
-    entry: { type: String, required: true },
+    sides: { type: String, required: true },
+    ingredients: { type: String },
+    img: { type: String},
+    cooking1: { type: String},
+    cooking2: { type: String},
+    cooking3: { type: String},
+    cooking4: { type: String},
+    cooking5: { type: String}
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
